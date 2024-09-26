f5ac1cb55bea8dcb616adfa83f98d866adcab6f1
shimodi <am.shahmoradi93@gmail.com>
2024-09-26 18:48:01 +0330

    test endpoint

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main(){
     fmt.Println("hello world")
+    var app = fiber.New()
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("hello world from root")
+    })
+    app.Listen(":3000")
 }
