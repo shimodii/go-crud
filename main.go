2ca8a58a35c6309c613553a2844945dd01ae4008
shimodi <am.shahmoradi93@gmail.com>
2024-09-26 23:40:29 +0330

    handler function changed changed to seperated function instead of inline function

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func rootHandler(c *fiber.Ctx) error {
+    return c.SendString("hello")
+}
+
 func main(){
     fmt.Println("hello world")
+    
     var app = fiber.New(fiber.Config{
         ServerHeader: "go-crud",
         AppName: "go crud application",
     })
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("hello world from root")
-    })
+    
+    app.Get("/", rootHandler)
+    
     app.Listen(":3000")
 }
